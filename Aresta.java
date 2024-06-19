78d6d17d8f3848479a728a473b9dc1c61889d354
gabriel-aires09 <aires514@gmail.com>
2024-06-19 11:37:21 -0300

    Removendo arquivos anteriores

--- Aresta.java
+++
@@ -1,9 +0,0 @@
-class Aresta {
-    No no;
-    int custo;
-
-    Aresta(No no, int custo) {
-        this.no = no;
-        this.custo = custo;
-    }
-}
--- NoCusto.java
+++
@@ -1,9 +0,0 @@
-public class NoCusto {
-    public No no;
-    public int custo;
-
-    public NoCusto(No no, int custo) {
-        this.no = no;
-        this.custo = custo;
-    }
-}
